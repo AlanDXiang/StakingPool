22237b0349ae1d997c4bb502158324ea940864ab
AlanDXiang <dongxuxiang1024@gmail.com>
2026-02-07 15:17:47 +0800

    update comment

--- scripts/interact.js
+++ scripts/interact.js
@@ -2,7 +2,7 @@ const hre = require("hardhat");
 
 async function main() {
     // -- CONFIGURATION --
-    // These are the addresses from your deployment screenshot
+    // replace addresses from yours
     const RWD_ADDR = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
     const STK_ADDR = "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512";
     const POOL_ADDR = "0x9fE46736679d2D9a65F0992F2272dE9f3c7fa6e0";
